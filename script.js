60903e7ce9a41ec321afe48bd74410f08f4712ae
Damaris Palacios <dslpz728@gmail.com>
2022-06-16 03:33:03 -0500

    starts js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-api key 7eda2dd9df64fe79bea81a1776e4212a
+const api_key="7eda2dd9df64fe79bea81a1776e4212a"
+const base_url="https://api.openweathermap.org/data/2.5"
 
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}	
