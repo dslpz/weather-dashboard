2951fe022a7c0c839850e8e426d808b16a4d5619
Damaris Palacios <dslpz728@gmail.com>
2022-06-16 03:56:33 -0500

    didnt save before commit

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const api_key="7eda2dd9df64fe79bea81a1776e4212a"
 const base_url="https://api.openweathermap.org/data/2.5"
 
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}	
+
+
+const interval = setInterval(function () {
+    $(".dateTime").text(new Date ());
+}, 1000); 
+
+function toTitleCase(str) {
+    return str.replace(/(?:^|\s)\w/g, function (match){
+        return match.toUpperCase();
+    });
+}
